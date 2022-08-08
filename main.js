391e23b7e42cbc17ddba2d3cbcc435bd32be6b3c
Felipe Santos <fdms@icomp.ufam.edu.br>
2022-08-07 20:06:30 -0400

    modularizing start sampling function

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import express from 'express';
-import axios from 'axios';
 import { sendDataToTangle } from './tangle-devnet.js'
 import { createNewVaccinationData, confirmVaccineStatus, freezeScreenForCapture } from './interactive-menu.js';
+import { startSampling } from './start-sampling.js';
 import { EdgeCameraClassifier } from './camera-classifier.js';
 
 
@@ -11,43 +11,13 @@ const edgeCamera = new EdgeCameraClassifier();
 await edgeCamera.runClassifier();
 
 
-const projectId = process.env.EDGE_IMPULSE_PROJECT_ID;
-const deviceId = process.env.DEVICE_ID;
-const edgeImpulseKey = process.env.EDGE_IMPULSE_API_KEY;
 
-async function startSampling(expected) {
-
-    let url = `https://studio.edgeimpulse.com/v1/api/${projectId}/device/${deviceId}/start-sampling`;
-
-    let headers = { 
-            headers: {
-            'content-type': 'application/json',
-            'x-api-key': edgeImpulseKey
-            }
-    };
-    
-    
-    let payload = {
-        label: expected,
-        lengthMs: 0,
-        category: 'training',
-        intervalMs: 0,
-        sensor: 'Camera'
-    };
-    
-    try {
-        const edgeImpulseResponse = await axios.post(url, payload, headers);
-        return { status: edgeImpulseResponse.status, data: edgeImpulseResponse.data };
-        
-    } catch (error) {
-        return { status: error.response.status , data: error.response.data };
-    }
-
-}
 
 routes.post('/start-sampling', async (request, response) => {
 
-    const sampling = await startSampling('loaded_syringe');
+    const status = request.body.status;
+
+    const sampling = await startSampling(status);
 
     response.status(sampling.status).send(sampling.data)
 
---
+++ start-sampling.js
@@ -0,0 +1,39 @@
+import axios from 'axios';
+
+
+const projectId = process.env.EDGE_IMPULSE_PROJECT_ID;
+const deviceId = process.env.DEVICE_ID;
+const edgeImpulseKey = process.env.EDGE_IMPULSE_API_KEY;
+
+
+const startSampling = async (expected) => {
+
+    let url = `https://studio.edgeimpulse.com/v1/api/${projectId}/device/${deviceId}/start-sampling`;
+
+    let headers = { 
+            headers: {
+            'content-type': 'application/json',
+            'x-api-key': edgeImpulseKey
+            }
+    };
+    
+    
+    let payload = {
+        label: expected,
+        lengthMs: 0,
+        category: 'training',
+        intervalMs: 0,
+        sensor: 'Camera'
+    };
+    
+    try {
+        const edgeImpulseResponse = await axios.post(url, payload, headers);
+        return { status: edgeImpulseResponse.status, data: edgeImpulseResponse.data };
+        
+    } catch (error) {
+        return { status: error.response.status , data: error.response.data };
+    }
+
+}
+
+export { startSampling }
